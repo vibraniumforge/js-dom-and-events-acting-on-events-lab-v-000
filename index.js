84b167114d85b72d8511a42ae5a82d75b1b6e4b7
Greg Morgan <gregm9999@gmail.com>
2019-01-20 03:51:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input=document.querySelector("input")
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi(){
+  let employeeName=retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick(){
+  let submit=document.querySelector('input[type="submit"]');
+  submit.addEventListener("click", function(e) {
+    addNewElementAsLi();
+    resetInput();
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let myLi=document.querySelector("a");
+  let myUl=document.querySelector("ul");
+  myLi.addEventListener("click", function(e) {
+    myUl.innerHTML='';
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = '';
+}
