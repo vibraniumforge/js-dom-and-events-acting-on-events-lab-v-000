931b6b460ce5fbe442b9742195a80ef5e60521ce
Greg Morgan <gregm9999@gmail.com>
2019-01-20 03:47:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addNewElementAsLi(){
 }
 
 function addNewLiOnClick(){
-  let submit=document.querySelector('input [type="submit"]');
+  let submit=document.querySelector('input[type="submit"]');
   submit.addEventListener("click", function(e) {
     addNewElementAsLi();
   })
