68c57db863e6dbd1b60519ea3434bb535f6a3a4d
Greg Morgan <gregm9999@gmail.com>
2019-01-20 03:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ let input=document.querySelector("input")
 function retrieveEmployeeInformation() {
   return input.value;
 }
-
-function addNewElementAsLi(){
-  let employeeName=retrieveEmployeeInformation();
-  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
-}
-
-function addNewLiOnClick(){
-  let submit=document.querySelector('input [type="submit"]');
-  submit.addEventListener("click", function(e) {
-    addNewElementAsLi();
-  })
-}
-
-function clearEmployeeListOnLinkClick(){
-  let link=document.querySelector("a");
-  let ul=document.querySelector("ul");
-  link.addEventListener("click", function(e) {
-    link.innerHTML='';
-  })
-}
-
-function resetInput(){
-  document.querySelector('input').value = ''
-}
+// 
+// function addNewElementAsLi(){
+//   let employeeName=retrieveEmployeeInformation();
+//   document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+// }
+// 
+// function addNewLiOnClick(){
+//   let submit=document.querySelector('input [type="submit"]');
+//   submit.addEventListener("click", function(e) {
+//     addNewElementAsLi();
+//   })
+// }
+// 
+// function clearEmployeeListOnLinkClick(){
+//   let link=document.querySelector("a");
+//   let ul=document.querySelector("ul");
+//   link.addEventListener("click", function(e) {
+//     link.innerHTML='';
+//   })
+// }
+// 
+// function resetInput(){
+//   document.querySelector('input').value = ''
+// }
